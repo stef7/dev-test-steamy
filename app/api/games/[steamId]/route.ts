83f84276c515b848179196bb0ac5d802b19b0a94
stef7 <1480673+stef7@users.noreply.github.com>
2024-01-25 23:15:15 +1100

    chore: add steam api usage tighter type, dedicated fn; our response type

--- app/api/games/[steamId]/route.ts
+++ app/api/games/[steamId]/route.ts
@@ -1,13 +1,31 @@
-import { steamApi } from "@/util/steam/api";
+import { getOwnedGamesInfo } from "@/util/steam/api";
 import { NextRequest, NextResponse } from "next/server";
+
+export type GamesResponse = {
+  totalCount: number;
+  totalHours: number;
+  gamesMostPlayed: {
+    name: string;
+    hours: number;
+  }[];
+};
 
 export async function GET(
   _req: NextRequest,
   { params: { steamId } }: { params: { steamId: string } },
 ) {
-  const info = await steamApi.getUserOwnedGames(steamId, {
-    includeExtendedAppInfo: true,
-  });
+  const games = await getOwnedGamesInfo(steamId);
 
-  return NextResponse.json(info);
+  const response: GamesResponse = {
+    totalCount: games.length,
+    totalHours: games.reduce((result, game) => result + game.minutes, 0) / 60,
+    gamesMostPlayed: games
+      .map(({ game: { name }, minutes }) => ({
+        name,
+        hours: minutes / 60,
+      }))
+      .sort((a, b) => b.hours - a.hours),
+  };
+
+  return NextResponse.json(response);
 }
--- util/steam/api.ts
+++ util/steam/api.ts
@@ -1,8 +1,13 @@
 import SteamAPI from "steamapi";
+import type { UserPlaytime, GameInfo } from "steamapi";
 
 import { steamWebApiKey } from "@/util/environment";
 
-if (!steamWebApiKey) throw new Error('missing steamWebApiKey');
+if (!steamWebApiKey) throw new Error("missing steamWebApiKey");
 
 export const steamApi = new SteamAPI(steamWebApiKey);
 
+export const getOwnedGamesInfo = (steamId: string) =>
+  steamApi.getUserOwnedGames(steamId, {
+    includeAppInfo: true,
+  }) as Promise<UserPlaytime<GameInfo>[]>;
