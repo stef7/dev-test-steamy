895b4f5863b2dd4dbf7bd47e508a85eca024c769
stef7 <1480673+stef7@users.noreply.github.com>
2024-01-25 23:23:24 +1100

    chore: infer api return type; export fn to query data directly in app router

--- app/api/games/[steamId]/route.ts
+++ app/api/games/[steamId]/route.ts
@@ -1,31 +1,23 @@
 import { getOwnedGamesInfo } from "@/util/steam/api";
 import { NextRequest, NextResponse } from "next/server";
 
-export type GamesResponse = {
-  totalCount: number;
-  totalHours: number;
-  gamesMostPlayed: {
-    name: string;
-    hours: number;
-  }[];
+export type GamesResponse = Awaited<ReturnType<typeof getGames>>;
+
+export const getGames = async (steamId: string) => {
+  const games = await getOwnedGamesInfo(steamId);
+  return {
+    totalCount: games.length,
+    totalHours: games.reduce((result, game) => result + game.minutes, 0) / 60,
+    gamesMostPlayed: games
+      .toSorted((a, b) => b.minutes - a.minutes)
+      // .slice(0, 10) // to get first X most played. assuming all for now
+      .map(({ game: { name }, minutes }) => ({ name, hours: minutes / 60 })),
+  };
 };
 
 export async function GET(
   _req: NextRequest,
   { params: { steamId } }: { params: { steamId: string } },
 ) {
-  const games = await getOwnedGamesInfo(steamId);
-
-  const response: GamesResponse = {
-    totalCount: games.length,
-    totalHours: games.reduce((result, game) => result + game.minutes, 0) / 60,
-    gamesMostPlayed: games
-      .map(({ game: { name }, minutes }) => ({
-        name,
-        hours: minutes / 60,
-      }))
-      .sort((a, b) => b.hours - a.hours),
-  };
-
-  return NextResponse.json(response);
+  return NextResponse.json(await getGames(steamId));
 }
