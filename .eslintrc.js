4d737237339adc890e72dedd9dc774d210f706c3
stef7 <1480673+stef7@users.noreply.github.com>
2024-01-25 22:11:53 +1100

    chore: add eslint comments plugin so we explain why disabling rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: "next/core-web-vitals",
+  extends: ["next/core-web-vitals", "plugin:eslint-comments/recommended"],
+  rules: {
+    "eslint-comments/no-unused-disable": "error",
+    "eslint-comments/require-description": "error",
+  },
 };
 
 module.exports = config;
